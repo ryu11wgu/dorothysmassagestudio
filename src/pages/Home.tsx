b9ac922c7b128bec7b070ce4d54f164ea641f81c
Raymond <ryu11wgu@gmail.com>
2026-01-30 11:57:40 -0800

    Update business hours

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,12 +12,12 @@ import {} from "@mantine/core";
 
 export default function Home() {
   const elements = [
-    { day: "Monday", hours: "10:00 am - 7:00 pm" },
-    { day: "Tuesday", hours: "10:00 am - 7:00 pm" },
-    { day: "Wednesday", hours: "6:00 pm - 9:00 pm" },
-    { day: "Thursday", hours: "6:00 pm - 9:00 pm" },
-    { day: "Friday", hours: "10:00 am - 7:00 pm" },
-    { day: "Saturday", hours: "Closed" },
+    { day: "Monday", hours: "8:00 am - 8:00 pm" },
+    { day: "Tuesday", hours: "8:00 am - 8:00 pm" },
+    { day: "Wednesday", hours: "8:00 am - 8:00 pm" },
+    { day: "Thursday", hours: "8:00 am - 8:00 pm" },
+    { day: "Friday", hours: "8:00 am - 8:00 pm" },
+    { day: "Saturday", hours: "8:00 am - 8:00 pm" },
     { day: "Sunday", hours: "Closed" },
   ];
 
