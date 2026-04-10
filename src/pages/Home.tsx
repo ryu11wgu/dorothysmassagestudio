d2d2186a984eab2c2963d84f47b5a02cd6476507
Raymond <ryu11wgu@gmail.com>
2026-04-09 20:57:05 -0700

    Update business hours

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,12 +12,12 @@ import { HeroImageBackground } from "../components/HeroImageBackground";
 
 export default function Home() {
   const elements = [
-    { day: "Monday", hours: "8:00 am - 8:00 pm" },
-    { day: "Tuesday", hours: "8:00 am - 8:00 pm" },
-    { day: "Wednesday", hours: "8:00 am - 8:00 pm" },
-    { day: "Thursday", hours: "8:00 am - 8:00 pm" },
-    { day: "Friday", hours: "8:00 am - 8:00 pm" },
-    { day: "Saturday", hours: "8:00 am - 8:00 pm" },
+    { day: "Monday", hours: "8:00 am - 10:00 pm" },
+    { day: "Tuesday", hours: "8:00 am - 10:00 pm" },
+    { day: "Wednesday", hours: "7:00 pm - 10:00 pm" },
+    { day: "Thursday", hours: "8:00 am - 10:00 pm" },
+    { day: "Friday", hours: "8:00 am - 10:00 pm" },
+    { day: "Saturday", hours: "8:00 am - 10:00 pm" },
     { day: "Sunday", hours: "Closed" },
   ];
 
